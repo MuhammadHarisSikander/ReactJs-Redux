eb5023882773f65399b54e6f9326d960e6c2b15a
MuhammadHarisSikander <32656489+MuhammadHarisSikander@users.noreply.github.com>
2021-02-21 18:27:54 +0500

    Action and Combine reducer Added

---
+++ src/Services/Reducers/reducer.js
@@ -0,0 +1,16 @@
+import { ADD_TO_CART } from "../Constants";
+const intialState = {
+    cardData: []
+}
+export default function cardItems(state=intialState, action) {
+    switch (action.type) {
+        case ADD_TO_CART:
+            return {
+                ...state,
+                cardData: action.data
+            }
+            break;
+        default:
+            return  state
+    }
+}
